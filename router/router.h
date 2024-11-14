9946d51c6bf16dcd93ef93c482d3fa943d92febf
xw3037 <xw3037@node0.alex-tao1.eecse6894-pg0.utah.cloudlab.us>
2024-11-14 13:38:51 -0700

    saving

--- router/router.h
+++ router/router.h
@@ -3,32 +3,31 @@
 #include <string>
 #include <future>
 #include <vector>
-#include <random>
 
 #include "../memcache/memcache_struct.h"
 #include "../memcache/lock_free_queue.h"
 
 class ZmqRouter {
+    template <typename T>
+    using Func = int(*)(T*);
+    typedef std::vector<std::string> StrArray;
+
 public:
     ZmqRouter() {}
 
     template <typename T>
-    void set_rule(std::string listen_port, std::vector<std::string> dest_hosts, 
-        std::vector<std::string> dest_ports) 
+    void set_rule(std::string listen_port, StrArray dest_hosts, 
+        StrArray dest_ports, std::string protocol, Func<T> func) 
     {
         thread_pool_.push_back(std::async(std::launch::async,
-            run<T>, listen_port, dest_hosts, dest_ports, "tcp"
+            run<T>, listen_port, dest_hosts, dest_ports, protocol, func
         ));
     }
 
     template <typename T>
-    static void run(std::string listen_port, std::vector<std::string> dest_hosts, 
-        std::vector<std::string> dest_ports, std::string protocol) 
+    static void run(std::string listen_port, StrArray dest_hosts, 
+        StrArray dest_ports, std::string protocol, Func<T> func) 
     {
-        std::random_device rd;
-        std::mt19937 gen{rd()};
-        std::uniform_int_distribution<int> dist(0, 127);
-
         zmq::context_t ctx{1};
         WebQueuePull<T> router_queue(&ctx, listen_port, "*", protocol);
         WebQueuePush<T> dealer_queue(&ctx);
@@ -40,7 +39,7 @@ public:
         while (true) {
             T value;
             router_queue.dequeue(value);
-            dealer_queue.enqueue(value, dist(gen));
+            dealer_queue.enqueue(value, func(&value));
         }
     }
 
--- router/run.cc
+++ router/run.cc
@@ -1,5 +1,6 @@
 #include <thread>
 #include <chrono>
+#include <random>
 
 # include "router.h"
 
@@ -8,19 +9,29 @@ using namespace benchmark;
 ZmqRouter router;
 typedef std::vector<std::string> StrArray;
 
+int route_rule(MemcacheRequest* req) {
+    std::random_device rd;
+    std::mt19937 gen{rd()};
+    std::uniform_int_distribution<int> dist(0, 127);
+    return dist(gen);
+}
+
 int main() {
     std::string dest_host = "128.110.217.67";
     for (size_t i = 0; i < zmq_read_ports.size(); i++) {
         router.set_rule<MemcacheRequest>(zmq_read_ports[i], 
-            StrArray{dest_host}, StrArray{zmq_read_ports[i]});
+            StrArray(zmq_read_ports.size(), dest_host), 
+            zmq_read_ports, "tcp", route_rule);
     }
     for (size_t i = 0; i < zmq_read_txn_ports.size(); i++) {
         router.set_rule<MemcacheRequest>(zmq_read_txn_ports[i], 
-            StrArray{dest_host}, StrArray{zmq_read_txn_ports[i]});
+            StrArray(zmq_read_txn_ports.size(), dest_host), 
+            zmq_read_txn_ports, "tcp", route_rule);
     }
     for (size_t i = 0; i < zmq_write_ports.size(); i++) {
         router.set_rule<MemcacheRequest>(zmq_write_ports[i], 
-            StrArray{dest_host}, StrArray{zmq_write_ports[i]});
+            StrArray(zmq_write_ports.size(), dest_host), 
+            zmq_write_ports, "tcp", route_rule);
     }
     std::this_thread::sleep_for(std::chrono::hours(1000000));
     return 0;
