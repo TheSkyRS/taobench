a6f17cf86fcd69b3bec2a190d9700e6195f4d70b
xw3037 <xw3037@node0.alex-tao1.eecse6894-pg0.utah.cloudlab.us>
2024-11-14 09:03:34 -0700

    router

--- router/run.cc
+++ router/run.cc
@@ -1,3 +1,6 @@
+#include <thread>
+#include <chrono>
+
 # include "router.h"
 # include "../memcache/memcache_struct.h"
 
@@ -19,5 +22,8 @@ int main() {
         router.set_rule(zmq_write_ports[i], std::vector<std::string>{dest_host}, 
                         std::vector<std::string>{zmq_write_ports[i]});
     }
+    std::this_thread::sleep_for(std::chrono::hours(1000000));
     return 0;
 }
+
+// Compile with: g++ run.cc -pthread -lzmq -I../src
